84d77e54d9465b5a08231f02c0acae07edd9f920
HARSH KUMAWAT <115946158+harshk04@users.noreply.github.com>
2023-04-26 22:46:36 +0530

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+async function fetchData(type) {
+  let response = await fetch("skills.json");
+  
+   
+
+  const data = await response.json();
+  return data;
+}
+
+function showSkills(skills) {
+  let skillsContainer = document.getElementById("skillsContainer");
+  let skillHTML = "";
+  skills.forEach(skill => {
+      skillHTML += `
+      <div class="bar">
+            <div class="info">
+              <img src=${skill.icon} alt="skill" />
+              <span>${skill.name}</span>
+            </div>
+          </div>`
+  });
+  skillsContainer.innerHTML = skillHTML;
+}
+
+
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
